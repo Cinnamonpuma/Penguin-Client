912dce9b582df3e69302cd1814d6fee22969426c
Your Name <user@example.com>
2025-05-22 13:15:12 -0400

    initial commit

---
+++ src/client/kotlin/penguin/client/PenguinCientClient.kt
@@ -0,0 +1,9 @@
+package penguin.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object PenguinCientClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
